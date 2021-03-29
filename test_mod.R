7e4de0d86898550300a8b268afb0b9e5fb972f3c
Vladimir Aron <vladimiraron1@yahoo.fr>
2021-03-29 18:38:08 +0200

    new idea about eih

---
+++ test_mod.R
@@ -0,0 +1,22 @@
+library(pdftools)
+library(tidyverse)
+library(tabulizer)
+library(shiny)
+library(miniUI)
+library(googlesheets4)
+library(tidyr)
+library(officer)
+library(data.table)
+library(rJava)
+library(xlsx)
+library(readxl)
+library(data.table)
+library(dplyr)
+library(tidyverse)
+
+test_mod <- read_sheet("https://docs.google.com/spreadsheets/d/1uR60vInUAPX4LhcyF60HPfFelOHdvPMuEJpx34SVaKA/edit#gid=0")
+yes
+0
+str(test_mod)
+
+
